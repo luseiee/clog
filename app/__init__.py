5f2c4cd9284e50df5281f1116dca9c672859ddda
luseiee <luseiee@outlook.com>
2016-10-02 00:01:17 +0800

    'Mail'

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@ from config import config
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app(config_name):
 	app = Flask(__name__)
@@ -17,6 +19,7 @@ def create_app(config_name):
 	bootstrap.init_app(app)
 	moment.init_app(app)
 	db.init_app(app)
+	mail.init_app(app)
 
 	# register the blueprint to the application
 	from .main import main as main_blueprint
--- config.py
+++ config.py
@@ -7,6 +7,14 @@ class Config:
 	# The secret key is used in flask web form to prevent CSRF attack
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	MAIL_SERVER = 'smtp.126.com'
+	MAIL_PORT = 25
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'phillu@126.com'
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '7z8i0k33'
+	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+	FLASKY_MAIL_SENDER = 'Flasky Admin <phillu@126.com>'
+	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or '<phillu@126.com>'
 
 class DevelopmentConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
